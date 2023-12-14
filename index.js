d6af34d95d75daa9525992007450af0c7a676093
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-14 21:43:43 +0300

    Add functions and styles to rest of the sections

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const menuIcon = document.querySelector(".menu-icon");
+const menu = document.querySelector(".menu");
 
 function handleMenu(){
-    document.querySelector(".menu-icon").addEventListener("click", ()=>{
-        
+    menuIcon.addEventListener("click", ()=>{
+        menu.classList.toggle("show");
     })
+}
+
+function closeMenu(){
+    if(menu.classList.contains("show")){
+        menu.classList.remove("show");
+    }
 }
